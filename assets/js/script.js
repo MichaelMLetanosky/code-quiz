60a6d2fa646646a8381094c841f4fa1d5029abb0
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-04 16:19:54 -0400

    linked js, add listener and set up function area

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,14 @@
 //Global Variables
-
+var startBtn = document.querySelector("#start-button")
 
 //Script
+
+//ToDo Create function that starts game
+function codeQuiz() {
+    console.log("Read set go")
+}
+
+//Todo create function that starts timer
+
+//Todo create event listener on start button
+startBtn.addEventListener ("click", codeQuiz)
