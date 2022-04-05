d6b00e076a9f2e53f20ec86eebb8ebc8bd33a828
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-04 20:50:55 -0400

    Added logic for win screen if all questions are answered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,37 @@
 //Global Variables
-//Todo
-var startBtn = document.querySelector("#start-button")
+//Todo add global variables
+var startBtn = document.querySelector("#start-button");
+var answerOne = document.querySelector("#answerOne");
+var answerTwo = document.querySelector("#answerTwo");
+var answerThree = document.querySelector("#answerThree");
+var answerFour = document.querySelector("#answerFour");
+var currentQuestion = 0;
    
 //Lists of questions as an array
-var answerArray = ['This is the first question?','yes','no','maybe','all of the above','1'];
+var answerArray = [['This is the first question?','yes','no','maybe','all of the above','1'],
+    ['this is the second question?', 'I guess', 'ask again later', 'outlook bad', 'outlook good', '4']
+];
 
 //Script
 //Todo Create function that starts game
-function codeQuiz(x) {
+function gameInitialization () {
     //Test that function can be called
     console.log("Ready set go!");
     
     //Todo starts timer
     
     //Hides introduction and start button
-    document.querySelector(".intro-loss-container").style.display = "none";
+    document.querySelector(".intro-box").style.display = "none";
+    document.querySelector(".loss-box").style.display = "none";
+    document.querySelector("#start-button").style.display = "none";
 
-    //Adds text to body to display questions and 4 answers as button appropriate buttons
+    currentQuestion = 0;
+}
+
+//Function for populating the quiz question and answers from the array
+function codeQuiz(x) {
+    console.log("Here a question");
+
     //Sets first item of given array as the question text
     document.querySelector(".question").textContent = x[0];
 
@@ -28,9 +43,49 @@ function codeQuiz(x) {
         }
 };
 
+function checkOne () {
+    console.log("Is this one right?");
+
+    currentQuestion++;
+    if (currentQuestion < answerArray.length) {
+        codeQuiz(answerArray[currentQuestion]);    
+    } else {
+        finalScore ();
+    };
+};
+
+function finalScore() {
+    console.log("Thanks for playing!")
+
+    //Clears out the question and answer areas
+    document.querySelector(".question").textContent = "";
+
+    for (let i=1; i < 5; i++) {
+        var answer = document.querySelector("[data-number='" + i + "']");
+        answer.textContent = "";
+        };
+
+    document.querySelector(".win-container").style.display = "block";
+};
+
+
+function playerLoss() {
+    document.querySelector(".loss-box").style.display = "block";
+    document.querySelector("#start-button").style.display = "inline";
+};
+
 //Event listener on start button
 startBtn.addEventListener ("click", function(){
-    codeQuiz(answerArray)
+    gameInitialization ();
+    codeQuiz(answerArray[currentQuestion]);
 });
+
+//Event listener for answer buttons
+document.addEventListener('click', (e) => {
+    let element = e.target;
+    if(element.className == "answerButton") {
+        checkOne();
+    };
+})
 
 console.log("Initialized");
