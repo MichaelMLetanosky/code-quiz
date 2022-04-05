97ca0127b1c4c1da903b707ee4c13b2bd2eb28b6
Michael Letanosky <MichaelMLetanosky@gmail.com>
2022-04-04 22:57:52 -0400

    Created timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,39 @@
 //Global Variables
 //Todo add global variables
 var startBtn = document.querySelector("#start-button");
-var answerOne = document.querySelector("#answerOne");
-var answerTwo = document.querySelector("#answerTwo");
-var answerThree = document.querySelector("#answerThree");
-var answerFour = document.querySelector("#answerFour");
 var currentQuestion = 0;
+var currentTime = 90
    
 //Lists of questions as an array
-var answerArray = [['This is the first question?','yes','no','maybe','all of the above','1'],
+var answerArray = [
+    ['This is the first question?','yes','no','maybe','all of the above','1'],
     ['this is the second question?', 'I guess', 'ask again later', 'outlook bad', 'outlook good', '4']
 ];
 
-//Script
-//Todo Create function that starts game
+//Starts game using the first question
 function gameInitialization () {
     //Test that function can be called
     console.log("Ready set go!");
     
-    //Todo starts timer
+    //Timer
+    var currentTime = 10;
+    var elem = document.querySelector("#timer");
+    elem.innerHTML = currentTime;
     
+    var timerId = setInterval(countdown, 1000);
+    
+    function countdown() {
+      if (currentTime == 0) {
+        clearTimeout(timerId);
+        playerEnd();
+      } else {
+        currentTime--;
+        elem.innerHTML = currentTime;
+      }
+    }
+
     //Hides introduction and start button
     document.querySelector(".intro-box").style.display = "none";
-    document.querySelector(".loss-box").style.display = "none";
     document.querySelector("#start-button").style.display = "none";
 
     currentQuestion = 0;
@@ -40,33 +51,31 @@ function codeQuiz(x) {
         var answer = document.querySelector("[data-number='" + i + "']");
         answer.textContent = x[i];
         answer.style.display = "block";
-        }
+        };
 };
 
 //Checks answer then moves to next question
 function checkOne (x) {
-    console.log(x);
-    let answer = answerArray[currentQuestion]
-    console.log(answer[5])
+    //To look at current questions array
+    let answer = answerArray[currentQuestion];
 
-    if (x == answer[5]) {
-        console.log("correct")
-    } else {
-        console.log("wrong")
-    }
+    //Checks the pressed button against the stored answer
+    if (x !== answer[5]) {
+        console.log("Wrong");
+    };
 
     //Moves on to the next question
     currentQuestion++;
     if (currentQuestion < answerArray.length) {
         codeQuiz(answerArray[currentQuestion]);    
     } else {
-        playerWin ();
+        playerEnd ();
     };
 };
 
-//Ends the game with a win
-function playerWin() {
-    console.log("Thanks for playing!")
+//Ends the game
+function playerEnd() {
+    console.log("Thanks for playing!");
 
     //Clears out the question and answer areas
     document.querySelector(".question").textContent = "";
@@ -80,23 +89,6 @@ function playerWin() {
     document.querySelector(".win-container").style.display = "block";
 };
 
-//Ends the game with a loss
-function playerLoss() {
-    console.log("Better luck next time.")
-
-    //Clears out the question and answer areas
-    document.querySelector(".question").textContent = "";
-
-    for (let i=1; i < 5; i++) {
-        var answer = document.querySelector("[data-number='" + i + "']");
-        answer.textContent = "";
-    };
-
-    //Puts loss text onto page
-    document.querySelector(".loss-box").style.display = "block";
-    document.querySelector("#start-button").style.display = "inline";
-};
-
 //Event listener on start button
 startBtn.addEventListener ("click", function(){
     gameInitialization ();
@@ -106,10 +98,10 @@ startBtn.addEventListener ("click", function(){
 //Event listener for answer buttons
 document.addEventListener('click', (e) => {
     let element = e.target;
-    let dataNumb = e.target.getAttribute("data-number")
+    let dataNumb = e.target.getAttribute("data-number");
     if(element.className == "answerButton") {
         checkOne(dataNumb);
     };
-})
+});
 
 console.log("Initialized");
